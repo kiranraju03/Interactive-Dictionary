3fa38b554fd5a2610d419d7b7ddf8c3b05e2c0a6
kiranraju03 <kiran.raju.k.43@gmail.com>
2017-08-09 19:28:07 +0530

    returning a formal message for non-existance words

--- dictionary.py
+++ dictionary.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def definition(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word entered doesnt exist, try again!"
 
 word = input("Enter the word : ")
 
